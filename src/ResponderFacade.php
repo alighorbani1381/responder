cff4b6407a38217be83498ee226a87cf11baf9cb
Ali Ghorbani <alighorbani20002@gmail.com>
2021-10-31 15:25:24 +0330

    fixed the bug on missing dependencies

--- src/ResponderFacade.php
+++ src/ResponderFacade.php
@@ -5,7 +5,7 @@ namespace Alighorbani\Responder;
 use Imanghafoori\SmartFacades\Facade;
 
 /**
- * @see \Alighorbani\Responder\Foundation\Foundation\BaseResponder
+ * @see \Alighorbani\Responder\Foundation\BaseResponder
  * @method static successfulResponse(bool $success , null|array $data, string $messageTitle, int $statusCode)
  * @method static resourceResponse($resourceItems, string $messageOrAlias, string $resourceClass)
  */
